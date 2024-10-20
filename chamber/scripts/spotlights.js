8a42c971f594a1afb6cac7a5da0ab7ac6586b35b
Joan Cerepi <joan.cerepi@outlook.com>
2024-10-19 23:24:22 -0600

    added spotlights

---
+++ chamber/scripts/spotlights.js
@@ -0,0 +1,42 @@
+const spotlights = document.getElementById('spotlights');
+
+const displaySpotlights = (members) => {
+  const goldMembers = members.filter(member => member.membership === "3 (Gold)");
+  const randNumsPicked = [];
+  let html = '';
+  
+  while (randNumsPicked.length < 3 && randNumsPicked.length < goldMembers.length) {
+    const randomNumber = Math.floor(Math.random() * goldMembers.length);
+    if (randNumsPicked.includes(randomNumber)) continue;
+    randNumsPicked.push(randomNumber);
+  }
+  
+  randNumsPicked.forEach(randNum => {
+    const goldMember = goldMembers[randNum];
+    html += `
+      <section class="gold-member-card">
+        <h2>${goldMember.name}</h2>
+        <img src="${goldMember.images.image}" alt="${goldMember.name}" width="50" height="50" loading="lazy">
+        <p>${goldMember.phone}</p>
+        <p>${goldMember.address}</p>
+        <p><a href="${goldMember.website}" target="_blank">Website</a></p>
+        <p>Level ${goldMember.membership}</p>
+      </section>
+    `;
+  });
+
+  spotlights.innerHTML = html;
+};
+
+const getRandomSpotlights = async (path) => {
+  const response = await fetch(path);
+  try {
+    if (!response.ok) throw Error('Something went wrong with response from network.');
+    const memberData = await response.json();
+    displaySpotlights(memberData.members);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getRandomSpotlights('./members.json');
