7466d2e8b3542796d6bda7193dc11da0665ccc1e
Joan Cerepi <joan.cerepi@outlook.com>
2024-10-28 20:35:51 -0600

    bundled general site scripts into one single module

--- tasteedish/scripts/main.js
+++ tasteedish/scripts/main.js
@@ -1,7 +1,6 @@
 
 import { TDhamburgerMenu, menuToggles, scroll } from './menu.js';
 import { displayCurrentYear } from './getdates.js';
-import { showRecipes, getRandomCuisine } from './recipes.js';
 
 // HEADER & NAVIGATION
 TDhamburgerMenu.addEventListener('click', menuToggles);
@@ -9,6 +8,3 @@ document.addEventListener('scroll', scroll);
 
 // CURRENT YEAR IN THE FOOTER
 displayCurrentYear();
-
-// DYNAMICALLY UPDATE DOM & POPULATE RECIPE LIST
-showRecipes(`https://api.edamam.com/api/recipes/v2?cuisineType=${getRandomCuisine()}&type=public&app_id=73857a73&app_key=3728694996ee6dacc502eff2391f439c`);
